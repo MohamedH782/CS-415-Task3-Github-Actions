69230430ea5ef34068740464b5eb0222f4087e72
Mohamed Hisham <152233933+MohamedH782@users.noreply.github.com>
2025-10-16 16:59:47 +0300

    simple code to test the CI pipeline

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,20 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+    public static void main(String[] args) {
+        System.out.println("Starting CI pipeline test...");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        int result = addNumbers(3, 7);
+        System.out.println("Sum of 3 + 7 = " + result);
+
+        if (result == 10) {
+            System.out.println(" Build and test successful!");
+        } else {
+            System.out.println(" Test failed: Expected 10");
         }
+    }
+
+    public static int addNumbers(int a, int b) {
+        return a + b;
     }
 }
